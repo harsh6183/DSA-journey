b1448e2114a18339d48b0e33233e947f8a73bb8e
harsh6183 <harshnimbalkar6181@gmail.com>
2025-07-08 13:46:49 +0530

    Array changes

--- Basics/Arrays/SumofArray.java
+++ Basics/Arrays/SumofArray.java
@@ -5,10 +5,50 @@ import java.util.Scanner;
 public class SumofArray {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+
         int num1= scn.nextInt();
-        int[] arr= new int[num1];
-        for(int i=0; i<arr.length;i++){
-              arr[i] = scn.nextInt();
+        int[] arr1= new int[num1];
+        for(int i=0; i<arr1.length;i++){
+              arr1[i] = scn.nextInt();
+        }
+
+       int num2= scn.nextInt();
+        int[] arr2= new int[num2];
+        for(int i=0; i<arr2.length;i++){
+              arr2[i] = scn.nextInt();
+        }
+
+        int[] sum = new int [num1>num2? num1:num2];
+        int carry = 0;
+        
+        int i =arr1.length -1;
+        int j = arr2.length -1;
+        int k = sum.length -1;
+
+        while(k>=0){
+            int digit =carry;
+            if ( i>=0){
+                digit +=arr1[i];
+            }
+
+            if(j>=0){
+                digit+=arr2[j];
+            }
+
+            carry = digit/10;
+            digit = digit%10;
+            sum[k]=digit;
+
+            i--;
+            j--;
+            k--;
+        }
+
+        if(carry >0){
+            System.out.println(carry);
+        }
+        for(int val:sum){
+            System.out.println(val);
         }
     }
 }
