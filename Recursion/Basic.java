17e887135bc5b9643546780616bae299763f8066
harsh6183 <harshnimbalkar6181@gmail.com>
2025-07-14 20:27:07 +0530

    Change in code

--- Recursion/Basic.java
+++ Recursion/Basic.java
@@ -1,4 +1,4 @@
-//Print n numbers using recursion
+//Take the input n and print n times the name;git 
 
 import java.util.Scanner;
 
@@ -7,14 +7,16 @@ public class Basic {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         printIncreasing(n);
+       
     }
 
     public static void printIncreasing(int n){
+         String name = "Harsh";
         if (n ==0){
             return;
         }
 
         printIncreasing(n-1);
-        System.out.println(n);
+        System.out.println(name);
     }
 }
