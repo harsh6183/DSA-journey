5bfbaacd45be62de6765492fb0d5df184473e40c
harsh6183 <harshnimbalkar6181@gmail.com>
2025-07-17 22:18:16 +0530

    Added new numerical

---
+++ Recursion/SumOfDigit.java
@@ -0,0 +1,20 @@
+//Take the input n number and sum that digit number using recursion
+
+import java.util.Scanner;
+
+public class SumOfDigit{
+      public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int ans = sum( n);
+        System.out.println(ans);
+
+    }
+
+    public static int sum(int n){
+        if (n == 0){
+            return 0;
+        }
+        return (n%10) + sum(n/10);
+    }
+}
