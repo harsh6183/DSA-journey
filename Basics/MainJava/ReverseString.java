3cd309eeef33e6ddcf7c71a779ff7691a0663e26
harsh6183 <harshnimbalkar6181@gmail.com>
2025-08-01 23:15:31 +0530

    New program changes

--- Basics/MainJava/ReverseString.java
+++ Basics/MainJava/ReverseString.java
@@ -1,4 +1,16 @@
 
 public class ReverseString {
-    
+    public static void main(String[] args) {
+        String keys = "ABCD";
+        String rev = "";
+        char a[] = keys.toCharArray();
+        int length = a.length;
+
+        for (int i = length - 1; i >= 0; i--) {
+            rev = rev + a[i];
+        }
+
+        System.out.println(rev);
+    }
+
 }
