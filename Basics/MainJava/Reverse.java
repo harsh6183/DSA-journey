63f9ff87d4fd1f011efcb8fa32127fdfdc5a2513
harsh6183 <harshnimbalkar6181@gmail.com>
2025-08-01 22:50:08 +0530

    Added new Program changes

--- Basics/MainJava/Reverse.java
+++ Basics/MainJava/Reverse.java
@@ -3,16 +3,25 @@ public class Reverse {
     public static void main(String[] args) {
         // int [] arr ={1,2,3,4,5};
 
-//    Method 1     for(int i=arr.length;i>arr[0];i--){
-//             System.out.println(i);
-//         }
+        // Method 1 for(int i=arr.length;i>arr[0];i--){
+        // System.out.println(i);
+        // }
 
-
-        //Method 2
+        // Method 2
         // int n = 12345;
         // StringBuffer sb= new StringBuffer(n);
         // StringBuffer reverse = sb.reverse();
         // System.out.println(reverse);
+
+        // Method 3
+        // int num = 12345;
+        // int rev=0;
+        // while(num!=0){
+        // rev =rev*10+ num%10;
+        // num =num/10;
+
+        // }
+        // System.out.println(rev);
 
     }
 }
