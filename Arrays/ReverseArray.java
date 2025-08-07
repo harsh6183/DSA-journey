0b24e3846856c3e3b3d7a1ffa2e0f442fecec542
harsh6183 <harshnimbalkar6181@gmail.com>
2025-08-07 23:40:01 +0530

    New code in Array

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,29 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        reverse(arr);
+        display(arr);
+    }
+
+    public static void reverse(int[] arr) {
+        int i = 0;
+        int j = arr.length - 1;
+        while (i < j) {
+            int tem = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tem;
+            i++;
+            j--;
+        }
+    }
+
+    public static void display(int[] arr) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int val : arr) {
+            sb.append(val + " ");
+        }
+        System.out.println(sb);
+    }
+
+}
