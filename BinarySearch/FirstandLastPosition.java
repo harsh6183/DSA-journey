a82974ab5620f6d31e6e7b32103fb1bff65bf636
harsh6183 <harshnimbalkar6181@gmail.com>
2025-09-11 22:13:56 +0530

    add new code

--- BinarySearch/FirstandLastPosition.java
+++ BinarySearch/FirstandLastPosition.java
@@ -1,4 +1,34 @@
 //Leetcode problme 34.Find First and Last Position of Element in Sorted Array
 public class FirstandLastPosition {
-    
+     public int[] searchRange(int[] nums, int target) {
+        int res[] = new int [2];
+        int first = binarySearch(nums,target,true);
+        int last = binarySearch(nums,target,false);
+        res[0]=first;
+        res[1]=last;
+        return res;
+    }
+
+    static int binarySearch(int[] arr,int target, boolean isFirst){
+        int ans =-1;
+        int start =0;
+        int end = arr.length-1;
+
+        while(start<=end){
+            int mid = start +(end- start)/2;
+            if(arr[mid]==target){
+                ans=mid;
+                if(isFirst){
+                    end = mid-1;
+                }else{
+                    start=mid +1;
+                }
+            } else if(arr[mid]<target){
+                start=mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
 }
