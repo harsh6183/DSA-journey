190278acd2a970519847339825e6f3b7aaa26352
harsh6183 <harshnimbalkar6181@gmail.com>
2025-09-19 17:20:40 +0530

    add new problem

---
+++ TwoPointers/SuffixPrefixDelete.java
@@ -0,0 +1,11 @@
+//Leetcode 1750 Minimum Length of String After Deleting Similar Ends
+/*Input: s = "cabaabac"
+Output: 0
+Explanation: An optimal sequence of operations is:
+- Take prefix = "c" and suffix = "c" and remove them, s = "abaaba".
+- Take prefix = "a" and suffix = "a" and remove them, s = "baab".
+- Take prefix = "b" and suffix = "b" and remove them, s = "aa".
+- Take prefix = "a" and suffix = "a" and remove them, s = "". */
+public class SuffixPrefixDelete{
+
+}
