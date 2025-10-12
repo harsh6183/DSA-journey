56fe6d8989b52352e16b186c7cc636152c55311f
harsh6183 <harshnimbalkar6181@gmail.com>
2025-10-12 20:18:33 +0530

    add problem

---
+++ Arrays/Leetcode/InsertInterval.java
@@ -0,0 +1,31 @@
+//Leetcode 57 Insert Interval
+
+public class InsertInterval{
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        ArrayList<int[]> res= new ArrayList<>();
+        //Storing intervals before overlap
+        int i= 0;
+        int n= intervals.length;
+        while(i<n && intervals[i][1]< newInterval[0]){
+            res.add(intervals[i]);
+            i++;
+        }
+
+        //merge overlapping intervals if any
+        while(i<n && newInterval[1]>=intervals[i][0]){
+            newInterval[0]=Math.min(newInterval[0],intervals[i][0]);
+            newInterval[1]=Math.max(newInterval[1],intervals[i][1]);
+            i++;
+        }
+        res.add(newInterval);
+
+        //append remaining intervals
+         while(i<n){
+            res.add(intervals[i]);
+            i++;
+        }
+
+        return res.toArray(new int[res.size()][2]);
+    }
+}}
