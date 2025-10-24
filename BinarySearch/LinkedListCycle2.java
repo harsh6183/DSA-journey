84a0ab89b4fd3f308e82800424b205edcfef719e
Harsh Nimbalkar <152949420+harsh6183@users.noreply.github.com>
2025-10-24 19:50:43 +0530

    Create LinkedListCycle2.java

---
+++ BinarySearch/LinkedListCycle2.java
@@ -0,0 +1,35 @@
+//Leetcode 142. Linked List Cycle II
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast= fast.next.next;
+
+            if(fast==slow){
+                slow = head;
+                while(slow!=fast){
+                    slow = head.next;
+                    fast= head.next;
+                } 
+                return slow;
+            }
+        }
+        return null ;
+    }
+}
