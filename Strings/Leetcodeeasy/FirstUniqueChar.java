4fe2672f0b0ce095a0a3f24bfec5ca56b9059bc6
Harsh Nimbalkar <152949420+harsh6183@users.noreply.github.com>
2025-10-25 21:54:58 +0530

    Create FirstUniqueChar.java

---
+++ Strings/Leetcodeeasy/FirstUniqueChar.java
@@ -0,0 +1,15 @@
+//Leetcode 387. First Unique Character in a String
+class Solution {
+    public int firstUniqChar(String s) {
+      int [] arr= new int[26];
+      for(int i=0; i<s.length();i++){
+        arr[s.charAt(i)-'a']++;  //Using asci values 
+      }
+      for(int i=0;i<s.length();i++){
+        if(arr[s.charAt(i)-'a']==1){
+            return i;
+        }
+      }
+      return -1;
+    }
+}
