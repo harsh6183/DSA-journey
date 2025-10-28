d5f628cc97ed6f48eeecadc32e9fce429bd6b2c4
Harsh Nimbalkar <152949420+harsh6183@users.noreply.github.com>
2025-10-28 17:40:01 +0530

    Create MakeSubSequence.java

---
+++ TwoPointers/MakeSubSequence.java
@@ -0,0 +1,21 @@
+//Leetcode 2486. Append Characters to String to Make Subsequence
+
+class Solution {
+    public int appendCharacters(String s, String t) {
+        int i =0;
+        int j=0;
+        int n=s.length();
+        int m=t.length();
+
+        while(i<n && j<m){
+            if(s.charAt(i)==t.charAt(j)){
+                i++;
+                j++;
+            }else{
+                i++;
+            }
+           
+        }
+         return (m-j);
+    }
+}
