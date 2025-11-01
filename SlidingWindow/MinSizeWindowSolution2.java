e03dc87a8d95a659b45b8b9489ae724e61dbedb2
Harsh Nimbalkar <152949420+harsh6183@users.noreply.github.com>
2025-11-01 19:49:04 +0530

    Create MinSizeWindowSolution2.java

---
+++ SlidingWindow/MinSizeWindowSolution2.java
@@ -0,0 +1,21 @@
+//Leetcode 209 Minimum size subarray sum
+
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int curSum =0;
+        int left =0;
+        int minLength=Integer.MAX_VALUE;
+
+        for(int right =0;right<nums.length;right++){
+            curSum+=nums[right];
+            while(curSum>=target){
+                if(right-left+1< minLength){
+                    minLength=right-left+1;
+                }
+                curSum-=nums[left];
+                left++;
+            }
+        }
+        return minLength!=Integer.MAX_VALUE?minLength:0;
+    }
+}
