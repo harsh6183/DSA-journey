9750ec24f02c053652688d7f4b5a8df0a329cc9f
Harsh Nimbalkar <152949420+harsh6183@users.noreply.github.com>
2025-12-16 11:17:01 +0530

    Update 1ReverseString.java

--- Strings/Leetcodeeasy/1ReverseString.java
+++ Strings/Leetcodeeasy/1ReverseString.java
@@ -1,7 +1,18 @@
 public void main(string[] args){
   String str="Hello";
+
+  //Approach 1
   char[] ch = str.toCharArray();
   for(int i =ch.length-1;i>0;i--){
     System.out.println(ch[i]);
   }
+
+  //Approach 2
+  for(int i =str.length()-1;i>0;i--){
+    System.out.println(str.charAt[i]);
+  }
+
+  //Approach 3
+  StringBuffer sb = new StringBuffer(str);
+  System.out.println(sb.reverse());
 }
